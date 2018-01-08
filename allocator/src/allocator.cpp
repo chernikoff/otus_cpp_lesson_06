d47524ed5a33023fbefd02d32042f692d1f3b906
Aleksey Chernikov <chernikoff.alex@gmail.com>
2018-01-08 18:21:01 +0300

    Introduce compile time pool allocator

--- allocator/src/allocator.cpp
+++ allocator/src/allocator.cpp
@@ -1,13 +1,50 @@
 #include <iostream>
 #include <stdexcept>
+#include <map>
+#include <cassert>
+
+#include <allocator/pool_allocator.h>
+
+auto factorial(size_t num)
+{
+  size_t result = 1;
+  for(size_t i = 1; i <= num; ++i) {
+    result *= i;
+  }
+  return result;
+}
 
 int main(int /*argc*/, char const * /*argv*/[])
 {
-  try
-  {
+  try {
+    auto initialize_map = [](auto & map) {
+      for(size_t i = 0; i < 10; ++i) {
+        map.emplace(i, factorial(i));
+      }
+    };
+
+    {
+      std::map< size_t, size_t > map_default_alloc;
+      initialize_map(map_default_alloc);
+      assert(map_default_alloc.size() == 10);
+    }
+
+    {
+      std::map< size_t, size_t, std::less<>,
+                pool_allocator< std::pair< size_t const, size_t >, 10 >
+                > map_custom_alloc;
+      initialize_map(map_custom_alloc);
+      assert(map_custom_alloc.size() == 10);
+
+      for (auto begin = std::begin(map_custom_alloc), end = std::end(map_custom_alloc), it = begin;
+           it != end; ++it) {
+        if (it != begin) std::cout << '\n';
+        std::cout << it->first << ' ' << it->second;
+      }
+
+    }
   }
-  catch(std::exception const & e)
-  {
+  catch(std::exception const & e) {
     std::cerr << e.what() << std::endl;
     throw;
   }
--- allocator_lib/include/allocator/pool_allocator.h
+++ allocator_lib/include/allocator/pool_allocator.h
@@ -3,6 +3,10 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <array>
+#include <list>
+#include <algorithm>
+#include <cassert>
 
 template< typename T, size_t PageSize = 1024 >
 class pool_allocator
@@ -16,11 +20,14 @@ class pool_allocator
     using other = pool_allocator< U, PageSize >;
   };
 
-  inline pool_allocator() noexcept;
-  inline pool_allocator(pool_allocator const & other) noexcept;
-  inline pool_allocator(pool_allocator && other) noexcept;
+  inline pool_allocator() noexcept = default;
+  inline pool_allocator(pool_allocator const & other) noexcept = default;
+  inline pool_allocator(pool_allocator && other) noexcept = default;
 
-  inline ~pool_allocator() noexcept;
+  inline ~pool_allocator() noexcept = default;
+
+  inline pool_allocator & operator=(pool_allocator const & other) noexcept = default;
+  inline pool_allocator & operator=(pool_allocator && other) noexcept = default;
 
   inline T* allocate(size_t n, void const * hint = nullptr);
   inline void deallocate(T* p, size_t n) noexcept;
@@ -30,6 +37,73 @@ class pool_allocator
 
   template< typename U >
   inline void destroy(U * p);
+
+ private:
+  struct page
+  {
+    uint8_t mem[PageSize * sizeof(T)];
+    size_t allocated = 0;
+    size_t deallocated = 0;
+  };
+
+  std::list< page > pages_;
 };
+
+template< typename T, size_t PageSize >
+T* pool_allocator< T, PageSize >::allocate(size_t n, void const *)
+{
+  if (n > PageSize) throw std::bad_alloc();
+  auto it = std::find_if(std::begin(pages_), std::end(pages_),
+                         [&n](auto const & page) {
+                           if ((PageSize - page.allocated) >= n) return true;
+                           return false;
+                         });
+
+  page * alloc_page = nullptr;
+  if (it == std::end(pages_)) {
+    pages_.emplace_back();
+    alloc_page = &pages_.back();
+  }
+  else {
+    alloc_page = &*it;
+  }
+
+  auto ptr = alloc_page->mem + alloc_page->allocated * sizeof(T);
+  alloc_page->allocated += n;
+  return reinterpret_cast< T* >(ptr);
+}
+
+template< typename T, size_t PageSize >
+void pool_allocator< T, PageSize >::deallocate(T *p, size_t n) noexcept
+{
+  auto ptr = reinterpret_cast< uintptr_t >(p);
+  for(auto it = std::begin(pages_), end = std::end(pages_);
+      it != end; ++it) {
+    auto start_addr = reinterpret_cast< uintptr_t >(it->mem);
+    auto end_addr = reinterpret_cast< uintptr_t >(it->mem + PageSize * sizeof(T));
+    if (start_addr <= ptr && ptr <= end_addr) {
+      it->deallocated += n;
+      if (it->deallocated == PageSize) {
+        pages_.erase(it);
+      }
+      return;
+    }
+  }
+  assert(false);
+}
+
+template< typename T, size_t PageSize >
+template< typename U, typename ... Args >
+void pool_allocator< T, PageSize >::construct(U *p, Args && ... args)
+{
+  new(p) U(std::forward< Args >(args) ...);
+}
+
+template< typename T, size_t PageSize >
+template< typename U >
+void pool_allocator< T, PageSize >::destroy(U *p)
+{
+  p->~U();
+}
 
 #endif /* POOL_ALLOCATOR_H */
